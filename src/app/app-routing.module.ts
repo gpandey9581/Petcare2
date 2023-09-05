db355f019c7073260a237cc8c9c71388965bded9
gpandey9581 <gauravpandeygkp7135615@gmail.com>
2023-09-05 19:19:27 +0530

    petcare2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/http-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  user = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private http:HttpClient) { }
+
+  SignUp():Observable<any>
+  {
+   return this.http.post('http://localhost:3000/user', this.user);
+  }
+
+  SignIn(email:string,password:string):Observable<any>
+  {
+  return this.http.get('http://localhost:3000/user');
+  }
+}
